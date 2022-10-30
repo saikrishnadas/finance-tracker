1929af580e442f97f052f375b7fb6e9806a86caf
Sai <sai@SAIs-MacBook-Pro.local>
2022-10-30 13:04:11 +0530

    fix 6

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -10,12 +10,11 @@ import { RootState } from "../store/index";
 import { ApiServicePost } from "../utils/ApiServices";
 import { useNavigate } from "react-router-dom";
 import { addCategory } from "../features/categorySlice";
-import { categories } from "../utils/day";
 
 function Categories() {
 	const [isOpen, setIsOpen] = useState(false);
 	const [isOpenEdit, setIsOpenEdit] = useState(false);
-	const [categories, setCategories] = useState<any>([]);
+	const [categories, setCategories] = useState([]);
 	const navigate = useNavigate();
 	const token = useSelector((state: RootState) => state.auth.token);
 	const dispatch = useDispatch();
@@ -56,12 +55,12 @@ function Categories() {
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				console.log("data", data);
+				// console.log("data", data);
 				if (data.message === "jwt expired") {
 					return navigate("/login");
 				}
-				setCategories(data);
-				// dispatch(addCategory(data));
+				setCategories(data.categories);
+				dispatch(addCategory(data.categories));
 			});
 	};
 
@@ -83,29 +82,30 @@ function Categories() {
 				<p>CATEGORIES</p>
 			</div>
 			<div className="flex flex-col gap-y-5 ml-5">
-				{categories.categories?.map((cat: any) => (
-					<span className="flex gap-x-3 items-center" key={cat._id}>
-						<div
-							className={`w-5 h-5 ${
-								cat.categories.color === "red" && "bg-red-500"
-							} ${cat.categories.color === "blue" && "bg-blue-500"} ${
-								cat.categories.color === "green" && "bg-green-500"
-							} ${cat.categories.color === "purple" && "bg-purple-500"} ${
-								cat.categories.color === "yellow" && "bg-yellow-500"
-							} ${cat.categories.color === "pink" && "bg-pink-500"}`}
-						/>
-						<p>{cat.categories.title}</p>
-						{/* <span className="cursor-pointer" onClick={openEditCategoryModal}>
+				{categories &&
+					categories?.map((cat: any) => (
+						<span className="flex gap-x-3 items-center" key={cat._id}>
+							<div
+								className={`w-5 h-5 ${
+									cat.categories.color === "red" && "bg-red-500"
+								} ${cat.categories.color === "blue" && "bg-blue-500"} ${
+									cat.categories.color === "green" && "bg-green-500"
+								} ${cat.categories.color === "purple" && "bg-purple-500"} ${
+									cat.categories.color === "yellow" && "bg-yellow-500"
+								} ${cat.categories.color === "pink" && "bg-pink-500"}`}
+							/>
+							<p>{cat.categories.title}</p>
+							{/* <span className="cursor-pointer" onClick={openEditCategoryModal}>
 								<EditIcon color="blue.600" />
 							</span> */}
-						<span
-							className="cursor-pointer"
-							onClick={() => deleteCategory(cat._id)}
-						>
-							<DeleteIcon color="red.500" />
+							<span
+								className="cursor-pointer"
+								onClick={() => deleteCategory(cat._id)}
+							>
+								<DeleteIcon color="red.500" />
+							</span>
 						</span>
-					</span>
-				))}
+					))}
 				<div
 					className="flex gap-x-3 items-center cursor-pointer text-blue-800 font-semibold hover:text-blue-400"
 					onClick={openAddCategoryModal}
